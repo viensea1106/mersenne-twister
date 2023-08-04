e5b76559ef99c5ee73a92697bbd66e9862c840e2
viensea1106 <viensea1106@gmail.com>
2023-08-04 23:03:07 +0700

    add demo python

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+class MT19937:
+    """
+        TODO: Python wrapper `mt19937.cpp` or maybe re-implement in python to crack 
+            random functions in python `random` module
+    """
+    pass
