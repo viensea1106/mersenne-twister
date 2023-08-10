f4c98400d6ed458b0b94b1880547853cca92697f
viensea1106 <viensea1106@gmail.com>
2023-08-10 21:30:37 +0700

    test state recovery attack

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import MersenneTwisterTools as MTT
 import random
 
-class MT19937:
-    """
-        TODO: Python wrapper `mt19937.cpp` or maybe re-implement in python to crack 
-            random functions in python `random` module
-    """
-    pass
+out = [random.getrandbits(32) for i in range(624)]
+cracker = MTT.state_recovery(algorithm="mt19937", output=out)
+
+for i in range(1337):
+    assert random.getrandbits(32) == cracker.next()
